fa75fa08b94ab32dfc380f70bcf06c39737ee07f
Marcelo <smarvic@gmail.com>
2025-06-09 20:55:44 -0300

    SENAC01

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  criarEntrevista: (dados) => ipcRenderer.invoke('criar-entrevista', dados),
-  listarEntrevistas: () => ipcRenderer.invoke('listar-entrevistas'),
-  deletarEntrevista: (id) => ipcRenderer.invoke('deletar-entrevista', id)
-});
